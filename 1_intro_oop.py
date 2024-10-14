333f350e006e90c803b07983c2938ff26dbc2336
Mohd Mehraj <mohammedmehrajuddin6@gmail.com>
2024-10-14 17:02:56 +0100

    OOPS

---
+++ 1_intro_oop.py
@@ -0,0 +1,13 @@
+# OOP = object oriented programming 
+
+# OOP is just a style/way to write a code
+# vry helpful in creating real world program
+# class,object(instance),method
+# list class
+# list object
+# method
+
+l = [1,2,3,4]
+l2 = [5,6,7]
+l3 = ['mehraj','khan']
+l.append(8)
